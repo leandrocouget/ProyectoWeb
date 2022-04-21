46a4025ae55ff7f98044680ffe7c238231ba9d3a
leandro couget <leandrocouget@gmail.com>
2022-04-21 11:47:55 -0300

    proyecto actualizado

---
+++ ProyectoWebApp/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+<<<<<<< HEAD
+=======
+from .models import register, articles, contact
+
+admin.site.register(register)
+admin.site.register(articles)
+admin.site.register(contact)
+
+>>>>>>> master
+
+# Register your models here.
